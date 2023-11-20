01c3fec3336b7c1b1ab6c262cb192ced00b8845f
Josh <leung.josh95@gmail.com>
2023-11-20 13:00:21 -0800

    initial project setup

---
+++ LunchTime/LunchTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchTimeApp.swift
+//  LunchTime
+//
+//  Created by Josh Leung on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LunchTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
