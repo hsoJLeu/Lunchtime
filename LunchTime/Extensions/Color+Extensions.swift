ec9ee884bcbe1bfe0ca1e3d37c400d0037578f17
Josh <leung.josh95@gmail.com>
2023-11-28 01:46:27 +0900

    extend button shape style to include custom green color

---
+++ LunchTime/Extensions/Color+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extensions.swift
+//  LunchTime
+//
+//  Created by Josh Leung on 11/27/23.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    public static var secondaryColor: Color {
+         return Color(red: 0.173, green: 0.337, blue: 0.004) // #2c5601
+    }
+}
