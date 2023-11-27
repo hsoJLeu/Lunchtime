537f8c2a0de85dc99492d2ffa6e5e59338a28fd9
Josh <leung.josh95@gmail.com>
2023-11-28 02:08:18 +0900

    Add Restaurant List View and updated initial app entry point

--- LunchTime/LunchTimeApp.swift
+++ LunchTime/LunchTimeApp.swift
@@ -9,9 +9,50 @@ import SwiftUI
 
 @main
 struct LunchTimeApp: App {
+    @StateObject var restaurantModel = MainViewModel()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ZStack(alignment: .bottom) {
+                    if restaurantModel.viewState == .list {
+                        RestaurantList(viewModel: restaurantModel)
+                    } else {
+                        MapView(viewModel: restaurantModel)
+                    }
+
+                    Button {
+                        if restaurantModel.viewState == .list {
+                            restaurantModel.viewState = .map
+                        } else {
+                            restaurantModel.viewState = .list
+                        }
+                    } label: {
+                        Label {
+                            restaurantModel.viewState == .list ? Text("Map").foregroundColor(.white) : Text("List").foregroundColor(.white)
+                        } icon: {
+                            restaurantModel.viewState == .list ? Image(systemName: "list.bullet")
+                                .foregroundColor(.white) : Image(systemName: "map").foregroundColor(.white)
+                        }
+                    }
+                    .frame(width: 113, height: 48)
+                    .background(.secondaryColor, in: Capsule())
+                    .padding()
+                }
+            }
+            .onAppear {
+                restaurantModel.getCurrentLocation()
+
+                Task {
+                    await restaurantModel.getNearbyRestaurants()
+                }
+            }
+            .searchable(text: $restaurantModel.searchQuery)
+            .onSubmit(of: .search) {
+                Task {
+                    await restaurantModel.getTextSearch()
+                }
+            }
         }
     }
 }
