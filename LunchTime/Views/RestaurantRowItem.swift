fe1d0e922d7633c62a4dfb3b6ff139b25dec3ec1
Josh <leung.josh95@gmail.com>
2023-11-29 00:18:40 +0900

    Update content detail view preview naming

--- LunchTime/Views/RestaurantRowItem.swift
+++ LunchTime/Views/RestaurantRowItem.swift
@@ -65,7 +65,7 @@ struct RestaurantRowItem: View {
     }
 }
 
-struct ContentDetailView_Previews: PreviewProvider {
+struct RestaurantRowItem_Previews: PreviewProvider {
     static var previews: some View {
         RestaurantRowItem(item: .make())
     }
