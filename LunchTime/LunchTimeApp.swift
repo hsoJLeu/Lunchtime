8974ff6aac78e6e2ef4bcde8bb3b4a67f35cc5cd
Josh <leung.josh95@gmail.com>
2023-11-29 16:31:38 +0900

    Update entry point to use ListMapScreen

--- LunchTime/LunchTimeApp.swift
+++ LunchTime/LunchTimeApp.swift
@@ -13,46 +13,7 @@ struct LunchTimeApp: App {
 
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                ZStack(alignment: .bottom) {
-                    if restaurantModel.viewState == .list {
-                        RestaurantList(viewModel: restaurantModel)
-                    } else {
-                        MapView(viewModel: restaurantModel)
-                    }
-
-                    Button {
-                        if restaurantModel.viewState == .list {
-                            restaurantModel.viewState = .map
-                        } else {
-                            restaurantModel.viewState = .list
-                        }
-                    } label: {
-                        Label {
-                            restaurantModel.viewState == .list ? Text("Map").foregroundColor(.white) : Text("List").foregroundColor(.white)
-                        } icon: {
-                            restaurantModel.viewState == .list ? Image(systemName: "list.bullet")
-                                .foregroundColor(.white) : Image(systemName: "map").foregroundColor(.white)
-                        }
-                    }
-                    .frame(width: 113, height: 48)
-                    .background(.secondaryColor, in: Capsule())
-                    .padding()
-                }
-            }
-            .onAppear {
-                restaurantModel.getCurrentLocation()
-
-                Task {
-                    await restaurantModel.getNearbyRestaurants()
-                }
-            }
-            .searchable(text: $restaurantModel.searchQuery)
-            .onSubmit(of: .search) {
-                Task {
-                    await restaurantModel.getTextSearch()
-                }
-            }
+            ListMapScreen().environmentObject(restaurantModel)
         }
     }
 }
