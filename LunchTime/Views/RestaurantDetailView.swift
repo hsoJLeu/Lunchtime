d0d9e758c2028398cb4d7145de179cccf8853804
Josh <leung.josh95@gmail.com>
2023-11-30 19:07:51 +0900

    Add horizontal picture carousel to RestaurantDetailView

--- LunchTime/Views/RestaurantDetailView.swift
+++ LunchTime/Views/RestaurantDetailView.swift
@@ -8,31 +8,59 @@
 import SwiftUI
 
 struct RestaurantDetailView: View {
+    @EnvironmentObject var viewModel: MainViewModel
     var item: Place = .generateData()
-    
+
     var body: some View {
         NavigationView {
             VStack(alignment: .leading) {
+                if let photos = item.photos {
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(photos, id: \.self) { photo in
+                                if let builtPhotoUrl = viewModel.getBuiltPhotoUrl(photo.name),
+                                   let url = URL(string: builtPhotoUrl) {
+                                    AsyncImage(url: url) { image in
+                                        image
+                                            .resizable()
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                    .frame(width: 200, height: 200)
+                                    .cornerRadius(5)
+                                }
+                            }
+                        }
+                    }
+                }
+                Text(item.displayName.text)
+                    .font(.title)
+                    .lineLimit(2)
                 Text(item.formattedAddress)
-                    .font(.title2)
-
+                    .font(.title3)
                 RatingStackView(rating: item.rating?.description,
                                 ratingCount: item.userRatingCount?.description)
-
+                if let hours = item.currentOpeningHours,
+                   hours.openNow {
+                    Text("Open")
+                        .foregroundColor(.green)
+                        .bold()
+                } else {
+                    Text("Closed")
+                        .foregroundColor(.red)
+                        .bold()
+                }
+                Text("\(item.weekdayDescriptions?[0] ?? "")")
                 Text(item.editorialSummary?.text ?? "")
-                    .font(.caption)
-
+                    .font(.subheadline)
+                if let link = URL(string: item.googleMapsUri) {
+                    Link("Google Maps", destination: link)
+                }
                 Spacer()
-    //            if let link = URL(string: item.googleMapsUri) {
-    //                Link("Google Maps", destination: link)
-    //            }
 
             }
-            .navigationTitle(item.displayName.text)
-            .lineLimit(2)
-
+            .padding(10)
         }
-
     }
 }
 
